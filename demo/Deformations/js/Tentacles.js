fc0c4914af1f4644341a1610e8d44d27c7d5df85
gabriel <gabrieldausque@gmail.com>
2019-11-27 22:34:13 +0100

    First step for deformation test

--- demo/Deformations/js/Tentacles.js
+++ demo/Deformations/js/Tentacles.js
@@ -1,5 +1,57 @@
 import * as BlackSheepGameEngine from "./blacksheepgameengine-build.js";
 
+class DrawImageWithSinusoid extends BlackSheepGameEngine.DrawImageBehavior {
+    constructor(entity) {
+        super(entity)
+    }
+
+    createElement(entity, image, body, display) {
+        let element = document.createElement('div');
+        element.id = entity.id;
+        
+        element.style.zIndex = body.z;
+        element.style.width = body.width + "px";
+        element.style.height = body.height + "px";
+        element.style.position = "absolute";
+
+        for(let rowIndex = 0; rowIndex < 2; rowIndex++) {
+            let rowElement = document.createElement('div');
+            rowElement.style.height = body.height/2 + "px";
+            rowElement.style.width = body.width + "px";
+            
+            element.appendChild(rowIndex);
+        }
+
+        let css = entity.components['css'];
+        if (css) {
+            for (let property in css.styles) {
+                if (css.styles.hasOwnProperty(property)) {
+                    element.style[property] = css.styles[property];
+                }
+            }
+        }
+        
+        display.appendChild(element);
+        
+        const collision = entity.components['collision'];
+        if (collision && window.gameEngine.isDebug) {
+            let colElt = document.createElement("div");
+            colElt.id = "col" + entity.id;
+            colElt.style.borderColor = "red";
+            colElt.style.borderWidth = "2px";
+            colElt.style.borderStyle = "solid";
+            colElt.style.top = collision.collisionRectangle.top + "px";
+            colElt.style.left = collision.collisionRectangle.left + "px";
+            colElt.style.width = collision.collisionRectangle.width + "px";
+            colElt.style.height = collision.collisionRectangle.height + "px";
+            colElt.style.position = "absolute";
+            element.appendChild(colElt);
+        }
+
+        return element;
+    }
+}
+
 export default class Tentacles extends BlackSheepGameEngine.Entity {
     constructor() {
         super();
